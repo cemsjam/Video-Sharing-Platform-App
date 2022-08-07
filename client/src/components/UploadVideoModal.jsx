fbed62051c663246708e5389e2bee7a526e5e9f1
Cemre Kur <cemrekur@gmail.com>
2022-08-07 16:12:20 +0300

    after vid uploaded

--- client/src/components/UploadVideoModal.jsx
+++ client/src/components/UploadVideoModal.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
 import UploadIcon from '@mui/icons-material/Upload';
 import styled, { css } from 'styled-components';
@@ -36,8 +36,14 @@ const Header = styled.header`
 const Content = styled.div`
   padding: 1.5rem;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
+  ${(props) =>
+    props.variant === 'last-step' &&
+    css`
+      align-items: stretch;
+    `}
 `;
 const Button = styled.button`
   display: flex;
@@ -60,12 +66,40 @@ const Button = styled.button`
       font-weight: 500;
       letter-spacing: -0.5px;
     `}
+  ${(props) =>
+    props.variant === 'upload' &&
+    css`
+      padding-inline: 1rem;
+      height: 50px;
+      min-width: 36px;
+      width: 100%;
+      border-radius: 2px;
+      background-color: var(--sign-in-button-accent-color);
+      color: var(--text-color);
+      font-size: 1.25rem;
+      text-transform: uppercase;
+      font-weight: 500;
+
+      margin-top: auto;
+    `}
 `;
 const Label = styled.label`
   display: flex;
   flex-direction: column;
   align-items: center;
   cursor: pointer;
+  ${(props) =>
+    props.htmlFor === 'img' &&
+    css`
+      align-items: flex-start;
+      gap: 1rem;
+      width: fit-content;
+      span {
+        font-size: 1rem;
+        color: var(--secondary-color);
+        font-weight: bold;
+      }
+    `}
 `;
 const UploadIconContainer = styled.span`
   display: flex;
@@ -79,7 +113,7 @@ const UploadIconContainer = styled.span`
   border-radius: 50%;
   background-color: var(--main-content-bg-color);
 `;
-const Input = styled.input`
+const HiddenInput = styled.input`
   clip: rect(0 0 0 0);
   clip-path: inset(50%);
   height: 1px;
@@ -87,6 +121,41 @@ const Input = styled.input`
   position: absolute;
   white-space: nowrap;
   width: 1px;
+`;
+const InputSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  input,
+  label {
+    display: block;
+  }
+  label {
+    font-size: 1rem;
+    color: var(--secondary-color);
+    text-transform: capitalize;
+    font-weight: bold;
+  }
+  input {
+    background-color: var(--main-content-bg-color);
+    height: 50px;
+    padding-inline: 1rem;
+  }
+  margin-bottom: 1rem;
+`;
+const ThumbnailImageContainer = styled.div`
+  aspect-ratio: 1;
+  width: 200px;
+  background-color: var(--main-content-bg-color);
+  margin-right: auto;
+  border-radius: 4px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  span {
+    font-size: 2.5rem;
+    background-color: var(--background-color);
+  }
 `;
 const InfoSpan = styled.span`
   font-size: 0.95rem;
@@ -102,6 +171,9 @@ const InfoSpanLight = styled.span`
 `;
 
 const UploadVideoModal = ({ handleOpenVideoUpload }) => {
+  const [img, setImg] = useState(undefined);
+  const [video, setVideo] = useState(undefined);
+
   return (
     <Modal>
       <ModalContent>
@@ -113,21 +185,62 @@ const UploadVideoModal = ({ handleOpenVideoUpload }) => {
             <CloseOutlinedIcon fontSize="small" />
           </Button>
         </Header>
-        <Content>
-          <Label htmlFor="uploadVideo">
-            <UploadIconContainer>
-              <UploadIcon fontSize="inherit" />
-            </UploadIconContainer>
-            <InfoSpan>Drag And drop video files to upload</InfoSpan>
-            <InfoSpanLight>
-              Your videos will be private until you publish them
-            </InfoSpanLight>
-            <Input type="file" id="uploadVideo" />
-            <Button variant="select" type="button">
-              Select Files
-            </Button>
-          </Label>
-        </Content>
+        {!video ? (
+          <Content>
+            <Label htmlFor="uploadVideo">
+              <UploadIconContainer>
+                <UploadIcon fontSize="inherit" />
+              </UploadIconContainer>
+              <InfoSpan>Drag And drop video files to upload</InfoSpan>
+              <InfoSpanLight>
+                Your videos will be private until you publish them
+              </InfoSpanLight>
+              <HiddenInput
+                type="file"
+                id="uploadVideo"
+                onChange={(e) => setVideo(e.target.files[0])}
+              />
+              <Button variant="select" type="button">
+                Select Files
+              </Button>
+            </Label>
+          </Content>
+        ) : (
+          <Content variant="last-step">
+            <InputSection>
+              <label htmlFor="title">Video Title</label>
+              <input
+                type="text"
+                name=""
+                id="title"
+                placeholder="Enter a video title"
+              />
+            </InputSection>
+            <InputSection>
+              <label htmlFor="description">Video Description</label>
+              <input
+                type="text"
+                name=""
+                id="description"
+                placeholder="Enter a video description"
+              />
+            </InputSection>
+            <Label htmlFor="img">
+              <span>Thumbnail Picture</span>
+              <HiddenInput
+                id="img"
+                type="file"
+                onChange={(e) => setImg(e.target.files)}
+              />
+              <ThumbnailImageContainer>
+                <UploadIconContainer>
+                  <UploadIcon fontSize="inherit" />
+                </UploadIconContainer>
+              </ThumbnailImageContainer>
+            </Label>
+            <Button variant="upload">Upload</Button>
+          </Content>
+        )}
       </ModalContent>
     </Modal>
   );
